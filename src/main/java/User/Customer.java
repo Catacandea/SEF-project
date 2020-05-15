c926d9f0cb443ecbe1624b7dbafad955e1ee9a45
Cândea Cătălin <catalin.candea@student.upt.ro>
2020-05-16 02:03:32 +0300

    Implemented Customer class
    Implemented customer class for issues : #64, #66, #62

---
+++ src/main/java/User/Customer.java
@@ -0,0 +1,65 @@
+package User;
+
+import java.util.ArrayList;
+
+import Order.Order;
+import Order.OrderStatusEnum;
+import Order.Product;
+
+public class Customer extends User {
+	private Order actualOrder;
+	private ArrayList<Order> totalOrders = new ArrayList<Order>();
+	private String address;
+
+	public Customer(String username, String phoneNumber, String email, String address) {
+		super(username, username, email);
+		this.setAddress(address);
+	}
+
+	public String viewProducts(ArrayList<Product> product) {
+		String returnValue = "";
+		for (Product auxOrder : product) {
+			returnValue += auxOrder.toString();
+		}
+		return returnValue;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Order getActualOrder() {
+		return actualOrder;
+	}
+
+	public void addProduct(Product product) {
+		if (actualOrder == null) {
+			actualOrder = new Order(null, this);
+		}
+		actualOrder.addProduct(product);
+	}
+
+	public void cancelOrder() {
+		actualOrder.cancelOrder();
+		actualOrder.getDelivery().setOrder(null);
+	}
+
+	public void finishOrder(int mark) {
+		actualOrder.getDelivery().getReview(mark);
+		OrderStatusEnum status = OrderStatusEnum.FINISH;
+		actualOrder.setStatus(status);
+		totalOrders.add(actualOrder);
+		actualOrder = null;
+
+	}
+
+	public Order placeOrder() {
+		return actualOrder;
+
+	}
+
+}

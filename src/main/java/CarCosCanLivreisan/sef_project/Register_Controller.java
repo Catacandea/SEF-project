4eb6cfe1a93a795b3c18402dabc26f43ee729a88
Cândea Cătălin <catalin.candea@student.upt.ro>
2020-05-17 20:59:30 +0300

    Added dummy users in the db

--- src/main/java/CarCosCanLivreisan/sef_project/Register_Controller.java
+++ src/main/java/CarCosCanLivreisan/sef_project/Register_Controller.java
@@ -18,7 +18,7 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Register_Controller {
-	DiskDB db = new DiskDB();
+
 	@FXML
 	private TextField userCustomer;
 	@FXML
@@ -55,134 +55,6 @@ public class Register_Controller {
 	private TextField fromHour;
 	@FXML
 	private TextField toHour;
-<<<<<<< Updated upstream
-	
-	
-
-    public void BackHomeViewButoonClicked(ActionEvent event) throws IOException
-    {
-        Parent HomePageView = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        Scene HomePageScene = new Scene(HomePageView);
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(HomePageScene);
-        window.show();
-    }
-
-    public void RegisterSupplyButoonClicked(ActionEvent event) throws IOException
-    {
-        Parent RegisterSupplyView = FXMLLoader.load(getClass().getResource("RegisterSupply.fxml"));
-        Scene RegisterSupplyScene = new Scene(RegisterSupplyView);
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(RegisterSupplyScene);
-        window.show();
-    }
-    
-    public void RegisterDeliveryButoonClicked(ActionEvent event) throws IOException
-    {
-        Parent RegisterDeliveryView = FXMLLoader.load(getClass().getResource("RegisterDelivery.fxml"));
-        Scene RegisterDeliveryScene = new Scene(RegisterDeliveryView);
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(RegisterDeliveryScene);
-        window.show();
-    }
-    
-    public void RegisterCustomerButoonClicked(ActionEvent event) throws IOException
-    {
-        Parent RegisterCustomerView = FXMLLoader.load(getClass().getResource("RegisterCustomer.fxml"));
-        Scene RegisterCustomerScene = new Scene(RegisterCustomerView);
-        
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(RegisterCustomerScene);
-        window.show();
-    }
-    
-    public void BackRegisterViewButoonClicked(ActionEvent event) throws IOException
-    {
-        Parent RegisterView = FXMLLoader.load(getClass().getResource("Register.fxml"));
-        Scene RegisterScene = new Scene(RegisterView);
-
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(RegisterScene);
-        window.show();
-    }
-    
-    public void HomePageCustomer(ActionEvent event) throws IOException
-    {
-        Parent CustomerHomePageView = FXMLLoader.load(getClass().getResource("CustomerHomePage.fxml"));
-        Scene CustomerHomePageScene = new Scene(CustomerHomePageView);
-        
-        if(userCustomer.getText().equals("") || nameCustomer.getText().equals("") || passwordCustomer.getText().equals("")
-        		||phoneCustomer.getText().equals("") || emailCustomer.getText().equals("") || addressCustomer.getText().equals(""))
-        	AlertBox.display("Register","Please insert the data in every field");
-        
-        else 
-        {
-        DiskDB db = new DiskDB();
-        Customer customer = db.registerCustomer(userCustomer.getText(), passwordCustomer.getText(), nameCustomer.getText(), phoneCustomer.getText(), emailCustomer.getText(), addressCustomer.getText());
-        
-        //daca nu ii in baza de date si toate campurile is completate merge mai jos
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(CustomerHomePageScene);
-        window.show();
-        }
-        
-    }
-
-    public void HomePageSupply(ActionEvent event) throws IOException, TimeSlotException
-    {
-        Parent SupplyHomePageView = FXMLLoader.load(getClass().getResource("SupplyCompanyHomePage.fxml"));
-        Scene SupplyHomePageScene = new Scene(SupplyHomePageView);
-        
-        if(userSupply.getText().equals("") || passwordSupply.getText().equals("") ||phoneSupply.getText().equals("") 
-        		|| emailSupply.getText().equals("") || addressSupply.getText().equals("") || fromHour.getText().equals("") || toHour.getText().equals(""))
-        	AlertBox.display("Register","Please insert the data in every field");
-        
-        else 
-        {
-        DiskDB db = new DiskDB();
-        //sp == null
-        SupplierCompany sp = db.registerSupplierCompany(userSupply.getText(),passwordSupply.getText(), Integer.parseInt(fromHour.getText()), Integer.parseInt(toHour.getText()),phoneSupply.getText(),addressSupply.getText(), emailSupply.getText());
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(SupplyHomePageScene);
-        window.show();
-        }
-    }
-
-    public void HomePageDelivery(ActionEvent event) throws IOException, TimeSlotException
-    {
-        Parent DeliveryHomePageView = FXMLLoader.load(getClass().getResource("DeliveryCompanyHomePage.fxml"));
-        Scene DeliveryHomePageScene = new Scene(DeliveryHomePageView);
-	
-        if(userDelivery.getText().equals("") || passwordDelivery.getText().equals("")
-        		||phoneDelivery.getText().equals("") || emailDelivery.getText().equals(""))
-        	AlertBox.display("Register","Please insert the data in every field");
-        
-        else 
-        {
-        DiskDB db = new DiskDB();
-	    DeliveryCompany dc = db.registerDeliveryCompany(userDelivery.getText(),passwordDelivery.getText(),emailDelivery.getText(),phoneDelivery.getText());
-	        
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        window.setScene(DeliveryHomePageScene);
-        window.show();
-        }
-    }
-    
-   
-    public void LogOutButtonClicked(ActionEvent event) throws IOException {
-        Parent LogOutView = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        Scene LogOutScene = new Scene(LogOutView);
-
-        AlertBox.display("Sign out","Are you sure you want to sign out?");
-        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-       
-        window.show();
-        //window.close();
-    }
-=======
 
 	public void BackHomeViewButoonClicked(ActionEvent event) throws IOException {
 		Parent HomePageView = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
@@ -233,19 +105,22 @@ public class Register_Controller {
 		Parent CustomerHomePageView = FXMLLoader.load(getClass().getResource("CustomerHomePage.fxml"));
 		Scene CustomerHomePageScene = new Scene(CustomerHomePageView);
 
-		// printeaza ce ia din field-uri
-//        System.out.println(userCustomer.getText());
-//        System.out.println(passwordCustomer.getText());
-//        System.out.println(emailCustomer.getText());
-//        System.out.println(phoneCustomer.getText());
-//        System.out.println(addressCustomer.getText());
+		if (userCustomer.getText().equals("") || nameCustomer.getText().equals("")
+				|| passwordCustomer.getText().equals("") || phoneCustomer.getText().equals("")
+				|| emailCustomer.getText().equals("") || addressCustomer.getText().equals(""))
+			AlertBox.display("Register", "Please insert the data in every field");
 
-		Customer customer = new Customer(userCustomer.getText(), nameCustomer.getText(), phoneCustomer.getText(),
-				emailCustomer.getText(), addressCustomer.getText());
-		// daca nu ii in baza de date si toate campurile is completate merge mai jos
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		window.setScene(CustomerHomePageScene);
-		window.show();
+		else {
+			DiskDB db = new DiskDB();
+			Customer customer = db.registerCustomer(userCustomer.getText(), passwordCustomer.getText(),
+					nameCustomer.getText(), phoneCustomer.getText(), emailCustomer.getText(),
+					addressCustomer.getText());
+
+			// daca nu ii in baza de date si toate campurile is completate merge mai jos
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(CustomerHomePageScene);
+			window.show();
+		}
 
 	}
 
@@ -253,38 +128,40 @@ public class Register_Controller {
 		Parent SupplyHomePageView = FXMLLoader.load(getClass().getResource("SupplyCompanyHomePage.fxml"));
 		Scene SupplyHomePageScene = new Scene(SupplyHomePageView);
 
-		// printeaza ce ia din field-uri
-//      System.out.println(userSupply.getText());
-//      System.out.println(passwordSupply.getText());
-//      System.out.println(emailSupply.getText());
-//      System.out.println(phoneSupply.getText());
-//      System.out.println(addressSupply.getText());
-//      System.out.println(fromHour.getText());
-//      System.out.println(toHour.getText());    
+		if (userSupply.getText().equals("") || passwordSupply.getText().equals("") || phoneSupply.getText().equals("")
+				|| emailSupply.getText().equals("") || addressSupply.getText().equals("")
+				|| fromHour.getText().equals("") || toHour.getText().equals(""))
+			AlertBox.display("Register", "Please insert the data in every field");
 
-		SupplierCompany sp = db.registerSupplierCompany(userSupply.getText(), passwordSupply.getText(),
-				Integer.parseInt(fromHour.getText()), Integer.parseInt(toHour.getText()), phoneSupply.getText(),
-				addressSupply.getText(), emailSupply.getText());
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		window.setScene(SupplyHomePageScene);
-		window.show();
+		else {
+			DiskDB db = new DiskDB();
+			// sp == null
+			SupplierCompany sp = db.registerSupplierCompany(userSupply.getText(), passwordSupply.getText(),
+					Integer.parseInt(fromHour.getText()), Integer.parseInt(toHour.getText()), phoneSupply.getText(),
+					addressSupply.getText(), emailSupply.getText());
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(SupplyHomePageScene);
+			window.show();
+		}
 	}
 
-	public void HomePageDelivery(ActionEvent event) throws IOException {
+	public void HomePageDelivery(ActionEvent event) throws IOException, TimeSlotException {
 		Parent DeliveryHomePageView = FXMLLoader.load(getClass().getResource("DeliveryCompanyHomePage.fxml"));
 		Scene DeliveryHomePageScene = new Scene(DeliveryHomePageView);
 
-//	    System.out.println(userDelivery.getText());
-//	    System.out.println(passwordDelivery.getText());
-//	    System.out.println(emailDelivery.getText());
-//	    System.out.println(phoneDelivery.getText());
+		if (userDelivery.getText().equals("") || passwordDelivery.getText().equals("")
+				|| phoneDelivery.getText().equals("") || emailDelivery.getText().equals(""))
+			AlertBox.display("Register", "Please insert the data in every field");
 
-		DeliveryCompany dc = new DeliveryCompany(userDelivery.getText(), emailDelivery.getText(),
-				phoneDelivery.getText());
+		else {
+			DiskDB db = new DiskDB();
+			DeliveryCompany dc = db.registerDeliveryCompany(userDelivery.getText(), passwordDelivery.getText(),
+					emailDelivery.getText(), phoneDelivery.getText());
 
-		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		window.setScene(DeliveryHomePageScene);
-		window.show();
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(DeliveryHomePageScene);
+			window.show();
+		}
 	}
 
 	public void LogOutButtonClicked(ActionEvent event) throws IOException {
@@ -297,6 +174,5 @@ public class Register_Controller {
 		window.show();
 		// window.close();
 	}
->>>>>>> Stashed changes
 
 }

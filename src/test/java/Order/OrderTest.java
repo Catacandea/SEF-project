c747af574e936ad1a23dd6790edf9c59f84f274b
Cosmina Sas <sas.cosmina99@gmail.com>
2020-05-18 01:47:12 +0300

    Test - Customer & Order

---
+++ src/test/java/Order/OrderTest.java
@@ -0,0 +1,33 @@
+package Order;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OrderTest {
+
+    @Test
+    public void testAddProduct() {
+        Product p1 = new Product("tricou", null, null, 0);
+        Order test1 = new Order(null, null);
+        test1.addProduct(p1);
+        String expected = "The Order with the ID " + "0" + " products list: tricou\n";
+        String output = test1.toString();
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Product p1 = new Product("tricou", null, null, 0);
+        Order test = new Order(null, null);
+        test.addProduct(p1);
+        String expected = "The Order with the ID " + "1" + " products list: tricou\n";
+        String output = test.toString();
+        assertEquals(expected, output);
+        test.deleteProduct("tricou");
+        expected = "The Order with the ID " + "0" + " products list: ";
+        output = test.toString();
+        assertEquals(expected, output);
+
+    }
+}

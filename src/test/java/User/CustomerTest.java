9a6b468d935bff6cd50cada744d17a24e8f3d986
Cândea Cătălin <catalin.candea@student.upt.ro>
2020-05-18 03:42:50 +0300

    Jar added
    Not working..

--- src/test/java/User/CustomerTest.java
+++ src/test/java/User/CustomerTest.java
@@ -10,29 +10,28 @@ import Order.ProductEnum;
 
 public class CustomerTest {
 
-    @Test
-    public void testAddProduct() {
-        Product p1 = new Product("Tricou", null, ProductEnum.CLOTHING, 80);
-        Customer test = new Customer("username", "Fullname", "000000000", "username@yahoo.com", "Street");
-        Order order_before = test.getActualOrder();
-        assertEquals(null, order_before);
-        test.addProduct(p1);
-        String expected = "The Order with the ID 1" + " products list: Tricou\n";
-        Order order = test.getActualOrder();
+	@Test
+	public void testAddProduct() {
+		Product p1 = new Product("Tricou", null, ProductEnum.CLOTHING, 80);
+		Customer test = new Customer("username", "Fullname", "000000000", "username@yahoo.com", "Street");
+		Order order_before = test.getActualOrder();
+		assertEquals(null, order_before);
+		test.addProduct(p1);
+		String expected = "The Order with the ID 1" + " products list: Tricou\n";
+		Order order = test.getActualOrder();
 
-        assertEquals(expected, order.toString());
-    }
+		assertEquals(expected, order.toString());
+	}
 
-    @Test
-    public void testFinishOrder() {
-        Product p1 = new Product("Tricou", null, ProductEnum.CLOTHING, 80);
-        Customer test = new Customer("username", "Fullname", "000000000", "username@yahoo.com", "Street");
-        test.addProduct(p1);
-        System.out.println(test.getActualOrder());
-        DeliveryCompany someone = new DeliveryCompany(null, null, null);
-        test.getActualOrder().setDelivery(someone);
-        test.finishOrder(5);
-        assertEquals(null, test.getActualOrder());
+	@Test
+	public void testFinishOrder() {
+		Product p1 = new Product("Tricou", null, ProductEnum.CLOTHING, 80);
+		Customer test = new Customer("username", "Fullname", "000000000", "username@yahoo.com", "Street");
+		test.addProduct(p1);
+		DeliveryCompany someone = new DeliveryCompany(null, null, null);
+		test.getActualOrder().setDelivery(someone);
+		test.finishOrder(5);
+		assertEquals(null, test.getActualOrder());
 
-    }
+	}
 }
